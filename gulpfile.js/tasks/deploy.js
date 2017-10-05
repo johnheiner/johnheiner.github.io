7c0c9616bfd4e54a82a6b759b167ed48a861a0db
John Heiner <heiner.john@gmail.com>
2017-10-04 23:02:53 -0400

    favicon

--- gulpfile.js/tasks/deploy.js
+++ gulpfile.js/tasks/deploy.js
@@ -1,22 +1,24 @@
-var ghPages = require('gulp-gh-pages')
-var gulp    = require('gulp')
-var os      = require('os')
-var path    = require('path')
+// var ghPages = require('gulp-gh-pages');
+var ghPages = require('gulp-gh-pages-cname');
+var gulp = require('gulp');
+var os = require('os');
+var path = require('path');
 
 var deployTask = function() {
-  var pkg = require(path.resolve(process.env.PWD, 'package.json'))
+    var pkg = require(path.resolve(process.env.PWD, 'package.json'));
 
-  var settings = {
-    url: pkg.homepage,
-    src: path.resolve(process.env.PWD, PATH_CONFIG.finalDest, '**/*'),
-    ghPages: {
-      cacheDir: path.join(os.tmpdir(), pkg.name)
-    }
-  }
+    var settings = {
+        url: pkg.homepage,
+        src: path.resolve(process.env.PWD, PATH_CONFIG.finalDest, '**/*'),
+        ghPages: {
+            cacheDir: path.join(os.tmpdir(), pkg.name),
+            branch: 'master',
+            cname: 'johnheiner.com'
+        }
+    };
 
-  return gulp.src(settings.src)
-    .pipe(ghPages(settings.ghPages))
-}
+    return gulp.src(settings.src).pipe(ghPages(settings.ghPages));
+};
 
-gulp.task('deploy', ['build'], deployTask)
-module.exports = deployTask
+gulp.task('deploy', ['build'], deployTask);
+module.exports = deployTask;
