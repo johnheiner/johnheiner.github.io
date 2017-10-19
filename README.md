09ed1e558af9ce9c4e9ccc6ac77ffbe2097f7278
John Heiner <heiner.john@gmail.com>
2017-10-19 00:48:55 -0400

    initial portfolio commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Projects
+
+## need Images
+LCLD CRM
+    - Wires
+    - Strategy, Design, Dev
+    - images
+        - Featured
+        - Full Set
+        - Link to Prototype
+        
+- Call Federal
+    - strategy, dev
+    - images
+        - Featured
+        - Full Set
+
+- Medstar Union Memorial
+    - Wires
+    - Strategy, Design
+    - images
+        - Featured
+        - Full Set
+
+- Visible Proof
+    - no wires
+    - my role: strategy/design
+    - images
+        - Featured
+        - Full Set
+
+- Print n Play
+    - design, dev
+    - Images
+        - Featured
+        - Primary
+
+- ChenMed
+    - design, dev
+
+    
+- Movement Makers
+    - strategy, dev
+
+
+## Don't Need Images
+- The Winebow Group
+    - wires
+    - strategy, design, dev
+    - Link to COLAB
+- Unboxed Technology
+    - Wires
+    - Strategy, Design, Development
+    - Link to COLAB
+- LCLD
+    - strategy, design, dev
+    - Link to COLAB
+
+
+# Experiments / Playground
+- Mainstreet Station
+- Riverstat.us
--- gulpfile.js/tasks/images.js
+++ gulpfile.js/tasks/images.js
@@ -1,95 +1,114 @@
-if (!TASK_CONFIG.images) return
+if (!TASK_CONFIG.images) return;
 
-var browserSync = require('browser-sync')
-var changed = require('gulp-changed')
-var gulp = require('gulp')
-var path = require('path')
-var imagemin = require('gulp-imagemin')
-var imageResize = require('gulp-image-resize')
-var responsive = require('gulp-responsive')
-var rename = require('gulp-rename')
-var imageSizes = [360, 720, 1440, 2160, 2880]
+var browserSync = require('browser-sync');
+var changed = require('gulp-changed');
+var gulp = require('gulp');
+var path = require('path');
+var imagemin = require('gulp-imagemin');
+var imageResize = require('gulp-image-resize');
+var responsive = require('gulp-responsive');
+var rename = require('gulp-rename');
+// var imageSizes = [360, 720, 1440, 2160, 2880];
 // var $ = require('gulp-load-plugins')();
 
 var imagesTask = function() {
-
   var paths = {
-    src: path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.images.src, '**/*.{' + TASK_CONFIG.images.extensions + '}'),
-    dest: path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.images.dest)
-  }
-  var options = [{
-      width: 360,
-      rename: {
-        suffix: '-360',
-        extname: '.jpg',
-      },
-      quality: 90,
-      format: 'jpeg',
-    }, {
-      width: 720,
-      rename: {
-        suffix: '-720',
-        extname: '.jpg'
-      },
-      quality: 80,
-      format: 'jpeg',
-    }, {
-      width: 1440,
-      rename: {
-        suffix: '-1440',
-        extname: '.jpg',
-      },
-      quality: 80,
-      format: 'jpeg',
-    }, {
-      width: 2160,
-      rename: {
-        suffix: '-2160',
-        extname: '.jpg',
-      },
-      quality: 70,
-      format: 'jpeg',
-    }, {
-      width: 2880,
-      rename: {
-        suffix: '-2880',
-        extname: '.jpg',
-      },
-      quality: 60,
-      format: 'jpeg',
-    }, {
+    src: path.resolve(
+      process.env.PWD,
+      PATH_CONFIG.src,
+      PATH_CONFIG.images.src,
+      '**/*.{' + TASK_CONFIG.images.extensions + '}'
+    ),
+    dest: path.resolve(
+      process.env.PWD,
+      PATH_CONFIG.dest,
+      PATH_CONFIG.images.dest
+    )
+  };
+  var options = [
+    // {
+    //   width: 360,
+    //   rename: {
+    //     suffix: '-360',
+    //     extname: '.jpg'
+    //   },
+    //   quality: 90,
+    //   format: 'jpeg'
+    // },
+    // {
+    //   width: 720,
+    //   rename: {
+    //     suffix: '-720',
+    //     extname: '.jpg'
+    //   },
+    //   quality: 80,
+    //   format: 'jpeg'
+    // },
+    // {
+    //   width: 1440,
+    //   rename: {
+    //     suffix: '-1440',
+    //     extname: '.jpg'
+    //   },
+    //   quality: 80,
+    //   format: 'jpeg'
+    // },
+    // {
+    //   width: 2160,
+    //   rename: {
+    //     suffix: '-2160',
+    //     extname: '.jpg'
+    //   },
+    //   quality: 70,
+    //   format: 'jpeg'
+    // },
+    // {
+    //   width: 2880,
+    //   rename: {
+    //     suffix: '-2880',
+    //     extname: '.jpg'
+    //   },
+    //   quality: 60,
+    //   format: 'jpeg'
+    // },
+    {
       width: '100%',
       rename: {
-        extname: '.jpg',
+        extname: '.jpg'
       },
-      quality: 70,
-      format: 'jpeg',
-    }];
+      quality: 65,
+      format: 'jpeg'
+    }
+  ];
 
-  
-  return gulp.src([paths.src, , '*!README.md'])
-    // .pipe(changed(paths.dest)) // Ignore unchanged files
-    .pipe(responsive({
-      '**/*.jpg': options,
-      '**/*.png': options
-    }, {
-      withMetadata: false,
-      withoutEnlargement: false,
-      skipOnEnlargement: false,
-      // errorOnEnlargement: false,
-      // quality: 70,
-      // errorOnUnusedConfig: false,
-      // errorOnUnusedImage: false,
-      // Use progressive (interlace) scan for JPEG and PNG output
-      progressive: true,
-    }))
-    .pipe(changed(paths.dest))
-    .pipe(gulp.dest(paths.dest))
-    .pipe(browserSync.stream())
-}
-
-
-
+  return (
+    gulp
+      .src([paths.src, , '*!README.md'])
+      .pipe(changed(paths.dest)) // Ignore unchanged files
+      .pipe(
+        responsive(
+          {
+            '**/*.jpg': options,
+            '**/*.png': options
+          },
+          {
+            withMetadata: false,
+            withoutEnlargement: false,
+            skipOnEnlargement: false,
+            // errorOnEnlargement: false,
+            // quality: 70,
+            // errorOnUnusedConfig: false,
+            // errorOnUnusedImage: false,
+            // Use progressive (interlace) scan for JPEG and PNG output
+            progressive: true
+          }
+        )
+      )
+      // .pipe(changed(paths.dest))
+      .pipe(gulp.dest(paths.dest))
+      .pipe(browserSync.stream())
+  );
+};
 
 // var imagesTaskOLD = function () {
 //   var exclude = '!' + path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.images.src, TASK_CONFIG.images.excludeFolder);
@@ -146,7 +165,6 @@ var imagesTask = function() {
 //     resizeSrc: path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.images.resizeSrc, '**/resize/*.{png,jpg}'),
 //     resizeDest: path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.images.resizeDest),
 //   }
-
 
 //   return gulp.src(paths.resizeSrc)
 //     .pipe(responsive({
@@ -210,8 +228,8 @@ var imagesTask = function() {
 
 // }
 
-gulp.task('images', imagesTask)
+gulp.task('images', imagesTask);
 
 // gulp.task('image-resize', imagesResizeTask)
 // gulp.task('image-resize-new', imagesResizeNewTask)
-module.exports = imagesTask
+module.exports = imagesTask;
